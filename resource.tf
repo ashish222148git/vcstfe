0e98fbf28623ffbccb94fd7d52b4e3561c6e17e0
ashish222148git <ashish222148git@gmail.com>
2026-08-29 22:48:11 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
 }
-
+/*
 resource "aws_instance" "web" {
   ami = "ami-00d2dbb426772b03a"
   instance_type = "t3.micro"
@@ -10,3 +10,4 @@ resource "aws_instance" "web1" {
   ami = "ami-00d2dbb426772b03a"
   instance_type = "t3.micro"
 }
+*/
